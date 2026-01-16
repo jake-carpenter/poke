68889c7f47e6d83b5065d4215acd5ab79b7552a2
Jake Carpenter <jake@jakecarpenter.dev>
2026-01-16 15:38:27 -0700

    fix: Stop writing derived Type property to config

--- Poke/Runners/SqlServer.cs
+++ Poke/Runners/SqlServer.cs
@@ -1,8 +1,10 @@
+using System.Text.Json.Serialization;
 using Poke.Models;
 
 namespace Poke.Runners;
 
 public record SqlServer : Server
 {
+  [JsonIgnore]
   public override string Type => "SqlServer";
 }
