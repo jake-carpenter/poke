c5214a0d9ec7c60c434e1f6a90484bb190f3631b
Jake Carpenter <jake@jakecarpenter.dev>
2026-01-24 15:54:34 -0700

    refactor: Cleaner test data fixtures

---
+++ Poke.Tests/HttpServerSource.cs
@@ -0,0 +1,39 @@
+using Poke.Runners;
+
+namespace Poke.Tests;
+
+public record HttpServerSource
+{
+    public required IReadOnlyCollection<HttpServer> Servers { get; init; }
+}
+
+public static partial class DataSources
+{
+    public static Func<HttpServerSource> HttpServers()
+    {
+        return () => new HttpServerSource
+        {
+            Servers =
+            [
+                new HttpServer
+                {
+                    GroupName = "Test Group 1",
+                    Instance = "Test 1 Instance",
+                    Uri = new Uri("https://example1.com"),
+                },
+                new HttpServer
+                {
+                    GroupName = "Test Group 2",
+                    Instance = "Test 2 Instance",
+                    Uri = new Uri("https://example2.com"),
+                },
+                new HttpServer
+                {
+                    GroupName = "Test Group 1",
+                    Instance = "Test 3 Instance",
+                    Uri = new Uri("https://example3.com"),
+                }
+            ]
+        };
+    }
+}
---
+++ Poke.Tests/SqlServerSource.cs
@@ -0,0 +1,35 @@
+using Poke.Runners;
+
+namespace Poke.Tests;
+
+public record SqlServerSource
+{
+    public required IReadOnlyCollection<SqlServer> Servers { get; init; }
+}
+
+public static partial class DataSources
+{
+    public static Func<SqlServerSource> SqlServers()
+    {
+        return () => new SqlServerSource
+        {
+            Servers =
+            [
+                new SqlServer
+                {
+                    GroupName = "Test Group 3",
+                    Instance = "Test 3 Instance",
+                    ConnectionString =
+                        "Data Source=test3.com;Initial Catalog=test3;Integrated Security=True;",
+                },
+                new SqlServer
+                {
+                    GroupName = "Test Group 4",
+                    Instance = "Test 4 Instance",
+                    ConnectionString =
+                        "Data Source=test4.com;Initial Catalog=test4;Integrated Security=True;",
+                },
+            ]
+        };
+    }
+}
